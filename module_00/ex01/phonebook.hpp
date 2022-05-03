d033ad6c88eb0238445fb400e87cc8c0a0120973
Askeladd42 <piema.lam@gmail.com>
2022-05-03 15:22:34 +0200

    cpp module00 ex01 started

--- module_00/ex01/phonebook.hpp
+++ module_00/ex01/phonebook.hpp
@@ -6,7 +6,40 @@
 /*   By: plam <plam@student.42.fr>                  +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/05/03 15:04:43 by plam              #+#    #+#             */
-/*   Updated: 2022/05/03 15:04:46 by plam             ###   ########.fr       */
+/*   Updated: 2022/05/03 15:22:28 by plam             ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
+class PhoneBook
+{
+private:
+	/* data */
+public:
+	PhoneBook(/* args */);
+	~PhoneBook();
+};
+
+PhoneBook::PhoneBook(/* args */)
+{
+}
+
+PhoneBook::~PhoneBook()
+{
+}
+
+class Contact
+{
+private:
+	/* data */
+public:
+	Contact(/* args */);
+	~Contact();
+};
+
+Contact::Contact(/* args */)
+{
+}
+
+Contact::~Contact()
+{
+}
