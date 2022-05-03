6a3eaecf3c9edf049ef0fc84b07bfecb354b22a0
Askeladd42 <piema.lam@gmail.com>
2022-05-03 14:38:07 +0200

    cpp module00 started

--- module_00/ex00/megaphone.cpp
+++ module_00/ex00/megaphone.cpp
@@ -6,7 +6,21 @@
 /*   By: plam <plam@student.42.fr>                  +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/05/03 14:10:07 by plam              #+#    #+#             */
-/*   Updated: 2022/05/03 14:10:11 by plam             ###   ########.fr       */
+/*   Updated: 2022/05/03 14:36:12 by plam             ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
+#include <iostream>
+
+int	main(int ac, char **av)
+{
+	if (ac > 1)
+	{
+		int	i = 1;
+		while (i < ac)
+			std::cout << av[i++];
+	}
+	else
+		std::cout << "* LOUD AND UNBEARABLE FEEDBACK NOISE *";
+	std::cout << '\n';
+}
