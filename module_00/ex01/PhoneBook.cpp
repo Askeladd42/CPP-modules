805569db214bdad8b6fdf62680df1922989cfa75
Askeladd42 <piema.lam@gmail.com>
2022-05-05 14:38:21 +0200

    cpp module00 ex01 started

--- module_00/ex01/PhoneBook.cpp
+++ module_00/ex01/PhoneBook.cpp
@@ -6,7 +6,7 @@
 /*   By: plam <plam@student.42.fr>                  +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/05/03 15:04:15 by plam              #+#    #+#             */
-/*   Updated: 2022/05/05 14:31:15 by plam             ###   ########.fr       */
+/*   Updated: 2022/05/05 14:37:52 by plam             ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -16,7 +16,10 @@ int	main()
 {
 	std::string	cmd;
 
-	std::cout << "Enter a command for the Phonebook : ";
-	std::cin >> cmd;
-	std::cout << "You entered the command " << cmd << '\n';
+	while (cmd != "EXIT")
+	{
+		std::cout << "Enter a command for the Phonebook : ";
+		std::cin >> cmd;
+		std::cout << "You entered the command " << cmd << '\n';
+	}
 }
