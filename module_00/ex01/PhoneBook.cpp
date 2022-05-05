a3348248b35343479b5b2519d7cf752bc1a287c2
Askeladd42 <piema.lam@gmail.com>
2022-05-05 14:55:25 +0200

    cpp module00 ex01 started

--- module_00/ex01/PhoneBook.cpp
+++ module_00/ex01/PhoneBook.cpp
@@ -6,20 +6,33 @@
 /*   By: plam <plam@student.42.fr>                  +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/05/03 15:04:15 by plam              #+#    #+#             */
-/*   Updated: 2022/05/05 14:37:52 by plam             ###   ########.fr       */
+/*   Updated: 2022/05/05 14:55:05 by plam             ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "PhoneBook.hpp"
 
-int	main()
+void	AddContact(class Phonebook)
+{
+	std::string contact;
+
+	std::cout << "Enter the new contact's name : ";
+	getline(std::cin, contact);
+}
+
+int		main()
 {
 	std::string	cmd;
+	class	Phonebook;
 
 	while (cmd != "EXIT")
 	{
 		std::cout << "Enter a command for the Phonebook : ";
 		std::cin >> cmd;
 		std::cout << "You entered the command " << cmd << '\n';
+		if (cmd == "ADD")
+			AddContact(PhoneBook);
+		if (cmd == "SEARCH")
+			SearchContact(PhoneBook);
 	}
 }
