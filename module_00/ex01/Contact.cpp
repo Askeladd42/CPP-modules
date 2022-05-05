30e40ef6a360c37ea0b58862fe0721b5fb0bbf22
Askeladd42 <piema.lam@gmail.com>
2022-05-05 17:23:20 +0200

    cpp module00 ex01 started& some modifications on ex00 to be more cpp functional

--- module_00/ex01/Contact.cpp
+++ module_00/ex01/Contact.cpp
@@ -1,31 +1,13 @@
 /* ************************************************************************** */
 /*                                                                            */
 /*                                                        :::      ::::::::   */
-/*   PhoneBook.hpp                                      :+:      :+:    :+:   */
+/*   Contact.cpp                                        :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
 /*   By: plam <plam@student.42.fr>                  +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
-/*   Created: 2022/05/03 15:04:43 by plam              #+#    #+#             */
-/*   Updated: 2022/05/05 17:22:26 by plam             ###   ########.fr       */
+/*   Created: 2022/05/05 17:19:52 by plam              #+#    #+#             */
+/*   Updated: 2022/05/05 17:21:35 by plam             ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
-#include <Contact.hpp>
-#include <iostream>
-
-class PhoneBook
-{
-private:
-	/* data */
-public:
-	PhoneBook(/* args */);
-	~PhoneBook();
-};
-
-PhoneBook::PhoneBook(/* args */)
-{
-}
-
-PhoneBook::~PhoneBook()
-{
-}
+#include "Contact.hpp"
--- module_00/ex01/Contact.hpp
+++ module_00/ex01/Contact.hpp
@@ -1,37 +1,31 @@
 /* ************************************************************************** */
 /*                                                                            */
 /*                                                        :::      ::::::::   */
-/*   megaphone.cpp                                      :+:      :+:    :+:   */
+/*   Contact.hpp                                        :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
 /*   By: plam <plam@student.42.fr>                  +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
-/*   Created: 2022/05/03 14:10:07 by plam              #+#    #+#             */
-/*   Updated: 2022/05/05 17:17:04 by plam             ###   ########.fr       */
+/*   Created: 2022/05/05 17:21:19 by plam              #+#    #+#             */
+/*   Updated: 2022/05/05 17:21:21 by plam             ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
-#include <iostream>
+#include <iomanip>
 #include <string>
 
-std::string	upper_str(std::string str)
+class Contact
 {
-	for (std::string::size_type i = 0; i < str.length(); i++)
-	{
-		if (std::islower(str[i]))
-			str[i] = std::toupper(str[i]);
-	}
-	return (str);
+private:
+	/* data */
+public:
+	Contact(/* args */);
+	~Contact();
+};
+
+Contact::Contact(/* args */)
+{
 }
 
-int	main(int ac, char **av)
+Contact::~Contact()
 {
-	if (ac > 1)
-	{
-		for (int i = 1; i < ac; i++)
-			std::cout << upper_str(av[i]);
-	}
-	else
-		std::cout << "* LOUD AND UNBEARABLE FEEDBACK NOISE *";
-	std::cout << std::endl;
-	return 0;
 }
