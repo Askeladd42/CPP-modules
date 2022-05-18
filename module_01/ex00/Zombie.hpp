3bc91dde233ad7b165d288f3104b81e811059caf
Askeladd42 <piema.lam@gmail.com>
2022-05-18 15:26:03 +0200

    ex02 started && module-01 started

---
+++ module_01/ex00/Zombie.hpp
@@ -0,0 +1,29 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   Zombie.hpp                                         :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: plam <plam@student.42.fr>                  +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2022/05/18 15:13:34 by plam              #+#    #+#             */
+/*   Updated: 2022/05/18 15:22:22 by plam             ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#ifndef ZOMBIE_HPP
+# define ZOMBIE_HPP
+
+# include <iostream>
+# include <string>
+
+class Zombie
+{
+private:
+	std::string	name;
+public:
+	Zombie();
+	~Zombie();
+	void	announce(void);
+};
+
+#endif
