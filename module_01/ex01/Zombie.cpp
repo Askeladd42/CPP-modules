7be6c5d01218bf3c8b74eb8c4844639f1d90b2a2
Askeladd42 <piema.lam@gmail.com>
2022-05-19 15:14:16 +0200

    ex02 started && module-01 started

---
+++ module_01/ex01/Zombie.cpp
@@ -0,0 +1,26 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   Zombie.cpp                                         :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: plam <plam@student.42.fr>                  +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2022/05/18 15:12:44 by plam              #+#    #+#             */
+/*   Updated: 2022/05/19 14:55:43 by plam             ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include "Zombie.hpp"
+
+Zombie::Zombie(std::string name) : _name(name) { }
+
+Zombie::~Zombie(void)
+{
+	std::cout << this->_name << " got obliterated !" << std::endl;
+}
+
+void	Zombie::announce()
+{
+	std::cout << this->_name << ": BraiiiiiiinnnzzzZ..." << std::endl;
+	return;
+}
---
+++ module_01/ex01/Zombie.hpp
@@ -0,0 +1,34 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   Zombie.hpp                                         :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: plam <plam@student.42.fr>                  +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2022/05/18 15:13:34 by plam              #+#    #+#             */
+/*   Updated: 2022/05/19 14:52:55 by plam             ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#ifndef ZOMBIE_HPP
+# define ZOMBIE_HPP
+
+# include <iostream>
+# include <string>
+
+class Zombie
+{
+private:
+	std::string	_name;
+
+public:
+	Zombie(std::string name);
+	~Zombie(void);
+	
+	void	announce(void);
+};
+
+Zombie*	newZombie(std::string name);
+void	randomChump(std::string name);
+
+#endif
