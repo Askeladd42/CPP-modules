2f82c0f3d4172033ffd866899e6f884c7e0208d9
Askeladd42 <piema.lam@gmail.com>
2022-05-19 17:43:22 +0200

    ex02 started && module-01 started

--- module_01/ex01/main.cpp
+++ module_01/ex01/main.cpp
@@ -6,7 +6,7 @@
 /*   By: plam <plam@student.42.fr>                  +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/05/18 15:11:47 by plam              #+#    #+#             */
-/*   Updated: 2022/05/19 16:36:00 by plam             ###   ########.fr       */
+/*   Updated: 2022/05/19 17:42:28 by plam             ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -15,10 +15,10 @@
 int	main(void)
 {
 	int	N = 20;
-	Zombie*	horde[N];
+	Zombie*	horde = zombieHorde(N, "Gary");
 
 	for (int i = 0; i < N; i++)
-		horde[i]->announce();
+		horde[i].announce();
 	delete [] horde;
 	return 0;
 }
--- module_01/ex01/zombieHorde.cpp
+++ module_01/ex01/zombieHorde.cpp
@@ -6,7 +6,7 @@
 /*   By: plam <plam@student.42.fr>                  +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/05/19 15:47:02 by plam              #+#    #+#             */
-/*   Updated: 2022/05/19 17:27:31 by plam             ###   ########.fr       */
+/*   Updated: 2022/05/19 17:40:42 by plam             ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -14,7 +14,7 @@
 
 Zombie*	zombieHorde(int N, std::string name)
 {
-	Zombie	horde[N];
+	Zombie*	horde = new Zombie[N];
 	for (int i = 0; i < N; i++)
 		horde[i].add_name(name);
 	return horde;
