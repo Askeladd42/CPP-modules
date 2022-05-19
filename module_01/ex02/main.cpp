7612084e06f5acb015e8f9fc7f66ab8c0db8558f
Askeladd42 <piema.lam@gmail.com>
2022-05-19 18:26:14 +0200

    module-01 ex02 finished

--- module_01/ex02/main.cpp
+++ module_01/ex02/main.cpp
@@ -6,7 +6,7 @@
 /*   By: plam <plam@student.42.fr>                  +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/05/18 15:11:47 by plam              #+#    #+#             */
-/*   Updated: 2022/05/19 18:04:31 by plam             ###   ########.fr       */
+/*   Updated: 2022/05/19 18:25:01 by plam             ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -16,5 +16,14 @@
 int	main(void)
 {
 	std::string	str = "HI THIS IS BRAIN";
-	std::cout << str << std::endl;
+	std::string* stringPTR = &str;
+	std::string& stringREF = str;
+
+	std::cout << "string memory address : " << &str << std::endl;
+	std::cout << "stringREF memory address : " << &stringPTR << std::endl;
+	std::cout << "stringREF memory address : " << &stringREF << std::endl;
+
+	std::cout << "string content : " << str << std::endl;
+	std::cout << "stringPTR content : " << stringPTR << std::endl;
+	std::cout << "stringREF content : " << stringREF << std::endl;
 }
