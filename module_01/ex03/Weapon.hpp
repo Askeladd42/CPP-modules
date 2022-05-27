863a33a73455f0684b611643b77452cd01d411a9
Askeladd42 <piema.lam@gmail.com>
2022-05-27 17:36:43 +0200

    module_00 ended, continuing module_01 & starting module_02

---
+++ module_01/ex03/Weapon.hpp
@@ -0,0 +1,30 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   Weapon.hpp                                         :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: plam <plam@student.42.fr>                  +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2022/05/27 16:59:40 by plam              #+#    #+#             */
+/*   Updated: 2022/05/27 17:24:38 by plam             ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#ifndef __WEAPON_HPP__
+# define __WEAPON_HPP__
+ 
+# include <string>
+
+class Weapon
+{
+private:
+	std::string _type;
+	
+public:
+	Weapon( void );
+	~Weapon( void );
+	static std::string const& getType( void );
+	static void setType(std::string newType);
+};
+
+#endif
---
+++ module_01/ex03/main.cpp
@@ -0,0 +1,37 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   main.cpp                                           :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: plam <plam@student.42.fr>                  +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2022/05/18 15:11:47 by plam              #+#    #+#             */
+/*   Updated: 2022/05/27 17:36:39 by plam             ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include <iostream>
+#include "Weapon.hpp"
+#include "HumanA.hpp"
+#include "HumanB.hpp"
+
+int main ( void ) {
+	{
+		Weapon	club = Weapon("crude spiked club");
+
+		HumanA bob("Bob", club);
+		bob.attack();
+		club.setType("some other type of club");
+		bob.attack();
+	}
+	{
+		Weapon	club = Weapon("crude spiked club");
+	
+		HumanB jim("Jim");
+		jim.setWeapon(club);
+		jim.attack();
+		club.setType("some other type of club");
+		jim.attack();
+	}
+	return 0;
+}
