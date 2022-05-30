de550dcfe8c994c02b3ffd6c242250add30eedfd
Askeladd42 <piema.lam@gmail.com>
2022-05-30 15:19:50 +0200

    continuing module_01 & starting module_02

---
+++ module_01/ex04/Replace.hpp
@@ -0,0 +1,28 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   Replace.hpp                                        :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: plam <plam@student.42.fr>                  +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2022/05/30 15:16:33 by plam              #+#    #+#             */
+/*   Updated: 2022/05/30 15:19:34 by plam             ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#ifndef __REPLACE_HPP__
+# define __REPLACE_HPP__
+
+# include <iostream>
+# include <string>
+
+class Replace
+{
+private:
+	/* data */
+public:
+	Replace(/* args */);
+	~Replace();
+};
+
+#endif
