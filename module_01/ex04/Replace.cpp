405ca7b93129130374589dcbd5ad6d5399a7a426
Askeladd42 <piema.lam@gmail.com>
2022-05-31 16:34:20 +0200

    continuing module_01 & starting module_02

--- module_01/ex04/Replace.cpp
+++ module_01/ex04/Replace.cpp
@@ -6,7 +6,7 @@
 /*   By: plam <plam@student.42.fr>                  +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/05/30 15:17:44 by plam              #+#    #+#             */
-/*   Updated: 2022/05/31 15:41:45 by plam             ###   ########.fr       */
+/*   Updated: 2022/05/31 16:30:08 by plam             ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -18,6 +18,7 @@ std::string	replaceStr( std::string dst, std::string s1, std::string s2 ) {
 	while (src != std::string::npos) {
 		dst.erase(src, s1.length());
 		dst.insert(src, s2);
+		src = dst.find(s1.c_str(), src + s2.length(), s1.length());
 	}
 	return dst;
 }
