a4e2428489ed2ddb9079d2c2bdd6fa1b26ad5b50
plam <plam@student.42.fr>
2022-06-02 15:24:01 +0200

    module_01 almost finished, module_02 started

--- module_02/ex00/Fixed.cpp
+++ module_02/ex00/Fixed.cpp
@@ -6,16 +6,30 @@
 /*   By: plam <plam@student.42.fr>                  +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/06/02 12:33:21 by plam              #+#    #+#             */
-/*   Updated: 2022/06/02 12:33:31 by plam             ###   ########.fr       */
+/*   Updated: 2022/06/02 15:23:01 by plam             ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "Fixed.hpp"
 
-Fixed::Fixed(/* args */)
-{
+Fixed::Fixed( void ) {
+	this->n = 0;
+	std::cout << "Default constructor called" << std::endl;
 }
 
-Fixed::~Fixed()
-{
+Fixed::Fixed(const Fixed&) {
+	std::cout << "Copy constructor called" << std::endl;
+}
+
+int		Fixed::getRawBits( void ) const {
+	std::cout << "getRawBits member function called" << std::endl;
+	return this->rawBits;
+}
+
+void	Fixed::setRawBits( int const raw ) {
+	this->n = raw;
+}
+
+Fixed::~Fixed( void ) {
+	std::cout << "Destructor called" << std::endl;
 }
--- module_02/ex00/Fixed.hpp
+++ module_02/ex00/Fixed.hpp
@@ -6,20 +6,28 @@
 /*   By: plam <plam@student.42.fr>                  +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/06/02 12:32:08 by plam              #+#    #+#             */
-/*   Updated: 2022/06/02 12:33:02 by plam             ###   ########.fr       */
+/*   Updated: 2022/06/02 15:20:49 by plam             ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #ifndef __FIXED_HPP__
 # define __FIXED_HPP__
 
+#include <iostream>
+
 class Fixed
 {
 private:
-	/* data */
+	int					n;
+	static int const	rawBits = 8;
+
 public:
-	Fixed(/* args */);
-	~Fixed();
+	Fixed( void );
+	Fixed( const Fixed&);
+	~Fixed( void );
+
+	int		getRawBits( void ) const;
+	void	setRawBits(int const raw );
 };
 
 #endif
