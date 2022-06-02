dcfdbab356415de5bd8f92ccd4613b0f7ff51e6e
plam <plam@student.42.fr>
2022-06-02 12:35:54 +0200

    module_01 almost finished, module_02 started

--- module_02/ex00/main.cpp
+++ module_02/ex00/main.cpp
@@ -6,8 +6,21 @@
 /*   By: plam <plam@student.42.fr>                  +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/06/02 12:31:38 by plam              #+#    #+#             */
-/*   Updated: 2022/06/02 12:32:00 by plam             ###   ########.fr       */
+/*   Updated: 2022/06/02 12:35:41 by plam             ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
+#include <iostream>
 #include "Fixed.hpp"
+
+int	main( void ) {
+	Fixed a;
+	Fixed b( a );
+	Fixed c;
+	
+	c = b;
+	std::cout << a.getRawBits() << std::endl;
+	std::cout << b.getRawBits() << std::endl;
+	std::cout << c.getRawBits() << std::endl;
+	return 0;
+}
