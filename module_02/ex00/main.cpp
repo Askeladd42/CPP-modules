76fc86ee77248a64c986d74ef6656ec82c6ba641
Askeladd42 <piema.lam@gmail.com>
2022-06-07 15:44:18 +0200

    module_02 started, ex00 done

--- module_02/ex00/main.cpp
+++ module_02/ex00/main.cpp
@@ -6,7 +6,7 @@
 /*   By: plam <plam@student.42.fr>                  +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/06/02 12:31:38 by plam              #+#    #+#             */
-/*   Updated: 2022/06/02 16:11:26 by plam             ###   ########.fr       */
+/*   Updated: 2022/06/07 15:42:58 by plam             ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -24,12 +24,14 @@ int	main( void ) {
 	std::cout << b.getRawBits() << std::endl;
 	std::cout << c.getRawBits() << std::endl;
 
+/* tests for modifications & assigment
 	a.setRawBits( 50 );
 	b.setRawBits( -25 );
 
 	std::cout << a.getRawBits() << std::endl;
 	std::cout << b.getRawBits() << std::endl;
 	std::cout << c.getRawBits() << std::endl;
+*/
 
 	return 0;
 }
