957c3876f6d667319fd904590d73f81cc3fa0603
Askeladd42 <piema.lam@gmail.com>
2022-06-08 15:21:12 +0200

    module_02 started, ex00 done

---
+++ module_02/ex02/Fixed.hpp
@@ -0,0 +1,42 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   Fixed.hpp                                          :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: plam <plam@student.42.fr>                  +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2022/06/02 12:32:08 by plam              #+#    #+#             */
+/*   Updated: 2022/06/08 14:28:17 by plam             ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#ifndef __FIXED_HPP__
+# define __FIXED_HPP__
+
+# include <iostream>
+# include <cmath>
+
+class Fixed
+{
+private:
+	int					_raw;
+	static int const	_bits = 8;
+
+public:
+	Fixed( void );
+	Fixed( int const nb );
+	Fixed( float const nb );
+	~Fixed( void );
+
+	Fixed( Fixed const &task );
+	Fixed& operator=( Fixed const &task );
+
+	int				toInt( void ) const;
+	float			toFloat( void ) const;
+	int				getRawBits( void ) const;
+	void			setRawBits(int const raw );
+};
+
+std::ostream&	operator<<( std::ostream &obj, Fixed const &nb );
+
+#endif
---
+++ module_02/ex02/main.cpp
@@ -0,0 +1,35 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   main.cpp                                           :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: plam <plam@student.42.fr>                  +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2022/06/02 12:31:38 by plam              #+#    #+#             */
+/*   Updated: 2022/06/07 16:25:21 by plam             ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include <iostream>
+#include "Fixed.hpp"
+
+int	main( void ) {
+	Fixed a;
+	Fixed b( 10 );
+	Fixed c( 42.42f );
+	Fixed d ( b );
+	
+	a = Fixed( 1234.4321f );
+	
+	std::cout << "a is " << a << std::endl;
+	std::cout << "b is " << b << std::endl;
+	std::cout << "c is " << c << std::endl;
+	std::cout << "d is " << d << std::endl;
+
+	std::cout << "a is " << a << " as integers" << std::endl;
+	std::cout << "b is " << b << " as integers" << std::endl;
+	std::cout << "c is " << c << " as integers" << std::endl;
+	std::cout << "d is " << d << " as integers" << std::endl;
+
+	return 0;
+}
