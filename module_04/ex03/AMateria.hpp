0ca4bcac7ac4b65be5636c8278def0b184de3019
Askeladd42 <piema.lam@gmail.com>
2022-07-29 18:48:12 +0200

    forgot the const in the use function of the AMateria class in the header

--- module_04/ex03/AMateria.hpp
+++ module_04/ex03/AMateria.hpp
@@ -6,7 +6,7 @@
 /*   By: plam <plam@student.42.fr>                  +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/07/18 18:30:19 by plam              #+#    #+#             */
-/*   Updated: 2022/07/29 18:27:03 by plam             ###   ########.fr       */
+/*   Updated: 2022/07/29 18:47:00 by plam             ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -29,7 +29,7 @@ class AMateria
 		std::string const &getType() const; //Returns the materia type
 		
 		virtual AMateria*	clone() const = 0;
-		virtual void		use( ICharacter	&target );
+		virtual void		use( ICharacter	&target ) const;
 };
 
 #endif
