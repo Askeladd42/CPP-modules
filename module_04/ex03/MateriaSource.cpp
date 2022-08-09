01cbdea60f8f9cc1b5a4f27654d5b13b58f676ed
Askeladd42 <piema.lam@gmail.com>
2022-08-09 13:12:52 +0200

    issues in the MateriaSource class solved, finishing the module04 and start the 05

--- module_04/ex03/MateriaSource.cpp
+++ module_04/ex03/MateriaSource.cpp
@@ -6,7 +6,7 @@
 /*   By: plam <plam@student.42.fr>                  +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/08/05 15:50:50 by plam              #+#    #+#             */
-/*   Updated: 2022/08/09 13:04:53 by plam             ###   ########.fr       */
+/*   Updated: 2022/08/09 13:12:45 by plam             ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -26,10 +26,21 @@ MateriaSource::~MateriaSource( void ) {
 }
 
 MateriaSource::MateriaSource( MateriaSource const &other ) {
-	
+	*this = other;
 }
 
 MateriaSource& MateriaSource::operator=( MateriaSource const &other ) {
+	if (this != &other) {
+		for (int i = 0; i < 4; i++ ) {
+			if (m_slot[i])
+				delete m_slot[i];
+			if (other.m_slot[i])
+				m_slot[i] = m_slot[i]->clone();
+			else
+				m_slot[i] = 0;
+		}
+	}
+	return *this;
 }
 
 void	MateriaSource::learnMateria(AMateria* m) {
