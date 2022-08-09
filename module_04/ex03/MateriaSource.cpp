b519a8a351081c3b8c6113d026f42c43b61b2cba
Askeladd42 <piema.lam@gmail.com>
2022-08-09 13:23:21 +0200

    issues in the MateriaSource class solved, finishing the module04 and start the 05

--- module_04/ex03/MateriaSource.cpp
+++ module_04/ex03/MateriaSource.cpp
@@ -6,7 +6,7 @@
 /*   By: plam <plam@student.42.fr>                  +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/08/05 15:50:50 by plam              #+#    #+#             */
-/*   Updated: 2022/08/09 13:12:45 by plam             ###   ########.fr       */
+/*   Updated: 2022/08/09 13:22:28 by plam             ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -48,9 +48,27 @@ void	MateriaSource::learnMateria(AMateria* m) {
 		std::cout << "No materia available for learning !" << std::endl;
 		return ;
 	}
+	for (int i = 0; i < 4; i++) {
+		if (m_slot[i] == 0) {
+			m_slot[i] = m;
+			return ;
+		}
+	}
+	std::cout << "This materia is already full ! It can't learn another magic type !" << std::endl;
+	delete m;
+	m = 0;
 }
 
 AMateria*	MateriaSource::createMateria( std::string const &type ) {
-	if (type == "")
+	if (type == "") {
+		std::cout << "Empty type passed : can't create a new materia." << std::endl;
 		return ;
+	}
+	for (int i = 0; i < 4; i++) {
+		if (m_slot[i] && type.compare(m_slot[i]->getType()) == 0) {
+			return m_slot[i]->clone();
+		}
+	}
+	std::cout << "The Materia Source didn't learn the type: " << type << "for some reasons ..." << std::endl;
+	return 0;
 }
