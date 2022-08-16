11058f83d773d49b671a5c6d495d58cdbf663cf3
Pierre-marie LAM <plam@e1r10p16.clusters.42paris.fr>
2022-08-16 15:22:26 +0200

    added missing headers in main file

--- module_04/ex03/AMateria.hpp
+++ module_04/ex03/AMateria.hpp
@@ -6,7 +6,7 @@
 /*   By: plam <plam@student.42.fr>                  +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/07/18 18:30:19 by plam              #+#    #+#             */
-/*   Updated: 2022/08/16 15:04:46 by plam             ###   ########.fr       */
+/*   Updated: 2022/08/16 15:21:57 by plam             ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -15,21 +15,19 @@
 
 # include "ICharacter.hpp"
 
-class AMateria
-{
-	protected:
-		std::string const	_type;
-	public:
-		AMateria( void );
-		AMateria( std::string const &type );
-		virtual	~AMateria( void );
-		AMateria( AMateria const &other );
-		AMateria &operator=( AMateria const &other );
-
-		std::string const &getType() const;
-		
-		virtual AMateria*	clone() const = 0;
-		virtual void		use( ICharacter	&target ) const;
+class AMateria {
+protected:
+	std::string const	_type;
+public:
+	AMateria( void );
+	AMateria( std::string const &type );
+	virtual	~AMateria( void );
+	AMateria( AMateria const &other );
+	AMateria &operator=( AMateria const &other );
+	
+	std::string const &getType() const;
+	virtual AMateria*	clone() const = 0;
+	virtual void		use( ICharacter	&target ) const;
 };
 
 #endif
