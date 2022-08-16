8c6b0fcdc81d34602482dabe5924a51959a605cb
Pierre-marie LAM <plam@e1r10p16.clusters.42paris.fr>
2022-08-16 15:38:34 +0200

    added missing headers in main file

--- module_04/ex03/Character.hpp
+++ module_04/ex03/Character.hpp
@@ -6,7 +6,7 @@
 /*   By: plam <plam@student.42.fr>                  +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/08/02 12:56:39 by plam              #+#    #+#             */
-/*   Updated: 2022/08/16 15:03:37 by plam             ###   ########.fr       */
+/*   Updated: 2022/08/16 15:38:19 by plam             ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -31,7 +31,7 @@ public:
 	Character( Character const &other );
 	Character &operator=( Character const &other );
 
-	std::string const & getName() const;
+	std::string const &getName() const;
 	void	equip( AMateria* m );
 	void	unequip( int idx );
 	void	use( int idx, ICharacter& target );
