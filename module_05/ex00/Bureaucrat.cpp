9379d1d75852ba6c282b6f0458f18df27745715d
Pierre-marie LAM <plam@e1r10p19.clusters.42paris.fr>
2022-08-17 15:50:34 +0200

    ex03 module_04 almost finished, need a fix on the clone funcctions, starting the module_05

--- module_05/ex00/Bureaucrat.cpp
+++ module_05/ex00/Bureaucrat.cpp
@@ -6,21 +6,24 @@
 /*   By: plam <plam@student.42.fr>                  +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/08/09 13:28:19 by plam              #+#    #+#             */
-/*   Updated: 2022/08/17 15:36:41 by plam             ###   ########.fr       */
+/*   Updated: 2022/08/17 15:50:25 by plam             ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "Bureaucrat.hpp"
 
-Bureaucrat::Bureaucrat( std::string name, int grade ) : _name(name) {
+Bureaucrat::Bureaucrat( std::string name, int grade ) {
 	try {
 		if (grade < 1)
 			throw Bureaucrat::GradeTooHighException;
 		if (grade > 150)
 			throw Bureaucrat::GradeTooLowException;
-		else
+		else {
+			_name = name;
+			_grade = grade;
 			std::cout << "New bureaucrat " << _name << " recruited in the "
 	 		<< "company with the grade " << _grade << std::endl;
+		}
 	}
 	catch ( std::exception &e ) {
 		std::cerr << &e << std::endl;
@@ -37,4 +40,19 @@ void	Bureaucrat::getName() {
 
 void	Bureaucrat::getGrade() {
 	std::cout << _grade << std::endl;
+}
+
+void	Bureaucrat::incrGrade() {
+	try {
+		if (this->_grade < 2)
+			throw Bureaucrat::GradeTooHighException;
+		else {
+			this->_grade--;
+			std::cout << "Bureaucrat " << _name << " 's grade hass been increased !"
+				<< std::endl;
+		}
+	}
+	catch ( std::exception &e ) {
+		std::cerr << &e << std::endl;
+	}
 }
