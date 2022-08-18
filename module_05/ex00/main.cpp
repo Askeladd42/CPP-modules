49e747c659a32dc421c704f611070f0e8c26221a
Pierre-marie LAM <plam@e1r9p17.clusters.42paris.fr>
2022-08-18 14:21:58 +0200

    module_05 ex01 started

--- module_05/ex00/main.cpp
+++ module_05/ex00/main.cpp
@@ -6,7 +6,7 @@
 /*   By: plam <plam@student.42.fr>                  +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/08/09 13:27:24 by plam              #+#    #+#             */
-/*   Updated: 2022/08/18 14:17:02 by plam             ###   ########.fr       */
+/*   Updated: 2022/08/18 14:21:43 by plam             ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -23,5 +23,6 @@ int	main( void ) {
 	Tom->decrGrade();
 	std::cout << Tom->getName() << std::endl;
 	std::cout << Tom->getGrade() << std::endl;
+
 	return 0;
 }
