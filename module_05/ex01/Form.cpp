43e2316b9c8a148c69327b2995a1a116310cbada
Pierre-marie LAM <plam@e1r9p17.clusters.42paris.fr>
2022-08-18 14:53:27 +0200

    module_05 ex01 started

--- module_05/ex01/Form.cpp
+++ module_05/ex01/Form.cpp
@@ -6,7 +6,7 @@
 /*   By: plam <plam@student.42.fr>                  +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/08/18 11:48:47 by plam              #+#    #+#             */
-/*   Updated: 2022/08/18 14:09:31 by plam             ###   ########.fr       */
+/*   Updated: 2022/08/18 14:37:23 by plam             ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -75,5 +75,14 @@ int	Form::getGradeSgn() {
 }
 
 void	Form::beSigned( Bureaucrat b ) {
-	
+	if ( this->getSgn() == false ) {
+		if ( this->getGradeSgn() >= b->getGrade())
+			std::cout << b->getName() << " signed " << this->getName()
+				<< std::endl;
+		else
+			std::cout << b->getName() << " couldn't sign " << this->getName()
+				<< " because " << std::endl;
+	}
+	else
+		std::cout << "The format " << this->getName() << " is already signed !" << std::endl;
 }
--- module_05/ex01/main.cpp
+++ module_05/ex01/main.cpp
@@ -6,7 +6,7 @@
 /*   By: plam <plam@student.42.fr>                  +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/08/09 13:27:24 by plam              #+#    #+#             */
-/*   Updated: 2022/08/17 16:44:43 by plam             ###   ########.fr       */
+/*   Updated: 2022/08/18 14:53:15 by plam             ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -14,12 +14,28 @@
 
 int	main( void ) {
 	Bureaucrat*	Tom = new Bureaucrat("Tom", 75);
-	Bureaucrat*	Jerry;
-	Bureaucrat*	Marcel;
+	Bureaucrat*	Jerry = new Bureaucrat("Jerry", 20);
+	Bureaucrat*	Marcel = new Bureaucrat("Marcel", 145);
 
-	Jerry = new Bureaucrat("Jerry", 200);
-	Marcel = new Bureaucrat("Marcel", -15);
 	Tom->incrGrade();
 	Tom->decrGrade();
+
+	Form*	Engram = new Form("Engram", 30, 25);
+	Engram->getSgn();
+	Engram->beSigned(*Tom);
+	Engram->beSigned(*Jerry);
+	Engram->beSigned(*Marcel);
+
+	Bureaucrat*	BigBoss = new Bureaucrat("Big Boss", 1);
+	Form*		Priority = new Form("Priority Task", 1, 1);
+
+	BigBoss->getName();
+	BigBoss->getGrade();
+	BigBoss->incrGrade();
+
+	Priority->beSigned(*Tom);
+	Priority->beSigned(*Jerry);
+	Priority->beSigned(*Marcel);
+	Priority->beSigned(*BigBoss);
 	return 0;
 }
