387e096013480ed8447218270d640cdf0fe05ffe
Pierre-marie LAM <plam@e1r9p17.clusters.42paris.fr>
2022-08-18 13:02:09 +0200

    module_05 ex02started

--- module_05/ex01/Form.cpp
+++ module_05/ex01/Form.cpp
@@ -6,9 +6,18 @@
 /*   By: plam <plam@student.42.fr>                  +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/08/18 11:48:47 by plam              #+#    #+#             */
-/*   Updated: 2022/08/18 11:50:02 by plam             ###   ########.fr       */
+/*   Updated: 2022/08/18 13:01:36 by plam             ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
-#include "Format.hpp"
+#include "Form.hpp"
 
+Form::Form( std::string name, int gradeExc, int gradeSgn ) {
+	
+}
+Form::~Form( void ) {
+	std::cout << "Format " << std::endl;
+}
+Form::Form( Form const &other );
+Form::Form	&operator=( Form const &other );
+Form::Form	&operator<<( Bureaucrat const &other );
--- module_05/ex01/Form.hpp
+++ module_05/ex01/Form.hpp
@@ -6,7 +6,7 @@
 /*   By: plam <plam@student.42.fr>                  +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/08/18 11:48:25 by plam              #+#    #+#             */
-/*   Updated: 2022/08/18 11:55:14 by plam             ###   ########.fr       */
+/*   Updated: 2022/08/18 13:01:47 by plam             ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -17,12 +17,35 @@
 
 class Form {
 	private:
-		std::string	_name const;
-		bool		_signed;
-		int			_gradeExc;
-		int			_gradeSgn;
+		std::string const	_name;
+		bool				_signed;
+		int					_gradeExc;
+		int					_gradeSgn;
 	public:
-		Form( void );
 		Form( std::string name, int gradeExc, int gradeSgn );
 		~Form();
+		Form( Form const &other );
+		Form	&operator=( Form const &other );
+		Form	&operator<<( Bureaucrat const &other );
+
+		class tooHigh : public std::exception {
+			virtual const char* what() const throw()
+			{
+				return "The grade assigned is too high !";
+			}
+		}GradeTooHighException;
+
+		class tooLow : public std::exception {
+			virtual const char* what() const throw()
+			{
+				return "The grade assigned is too low !";
+			}
+		}GradeTooLowException;
+		
+		void	beSigned( Bureaucrat b );
+
+		void	getName();
+		void	getGrade();
 };
+
+#endif
