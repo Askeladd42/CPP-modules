bde0cc2b5af39c30360d317be1fea4daa4f09a03
Pierre-marie LAM <plam@e1r11p14.clusters.42paris.fr>
2022-08-23 12:12:45 +0200

    ex02 module_05 started, modifying the already existent files to match the requirements given

--- module_05/ex01/main.cpp
+++ module_05/ex01/main.cpp
@@ -6,7 +6,7 @@
 /*   By: plam <plam@student.42.fr>                  +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/08/09 13:27:24 by plam              #+#    #+#             */
-/*   Updated: 2022/08/22 16:32:59 by plam             ###   ########.fr       */
+/*   Updated: 2022/08/23 12:11:18 by plam             ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -20,18 +20,19 @@ int	main( void ) {
 
 	Tom->incrGrade();
 	Tom->decrGrade();
+	
 	Form*	Engram = new Form("Engram", 30, 25);
 	std::cout << Engram->getSgn() << std::endl;
 	Tom->signForm(*Engram);
 	Jerry->signForm(*Engram);
 	Marcel->signForm(*Engram);
 	std::cout << Engram->getSgn() << std::endl;
+	delete (Engram);
 
 	Bureaucrat*	BigBoss = new Bureaucrat("Big Boss", 1);
 	Form*		Priority = new Form("Prior Task", 1, 1);
 
-	std::cout << BigBoss->getName() << std::endl;
-	std::cout << BigBoss->getGrade() << std::endl;
+	std::cout << *BigBoss;
 	BigBoss->incrGrade();
 
 	Tom->signForm(*Priority);
