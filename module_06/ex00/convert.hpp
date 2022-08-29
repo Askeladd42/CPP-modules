9dd131bb99fb6346d54b11b4a3cbff9b77c6adb6
Pierre-marie LAM <plam@e1r4p8.clusters.42paris.fr>
2022-08-29 13:54:11 +0200

    some modifications to match the subject in module_02 before corretion

--- module_06/ex00/convert.hpp
+++ module_06/ex00/convert.hpp
@@ -6,7 +6,7 @@
 /*   By: plam <plam@student.42.fr>                  +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/08/29 12:46:04 by plam              #+#    #+#             */
-/*   Updated: 2022/08/29 12:48:45 by plam             ###   ########.fr       */
+/*   Updated: 2022/08/29 13:54:00 by plam             ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -14,6 +14,7 @@
 # define __CONVERT_HPP__
 
 # include <iostream>
+# include <cmath>
 
 class convert
 {
