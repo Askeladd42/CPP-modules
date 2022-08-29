67c95477f83c71657e8b9df5c43e5a3c6232ec7b
Pierre-marie LAM <plam@e1r4p8.clusters.42paris.fr>
2022-08-29 18:42:04 +0200

    some modifications to match the subject in module_02 before corretion

--- module_02/ex03/bsp.cpp
+++ module_02/ex03/bsp.cpp
@@ -6,17 +6,25 @@
 /*   By: plam <plam@student.42.fr>                  +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/06/09 00:54:02 by plam              #+#    #+#             */
-/*   Updated: 2022/08/29 17:39:29 by plam             ###   ########.fr       */
+/*   Updated: 2022/08/29 18:41:22 by plam             ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "Fixed.hpp"
 #include "Point.hpp"
 
+int	range( Point const &a, Point &b ) {
+	Fixed	x = a.getX() - b.getX();
+	Fixed	y = a.getY() - b.getY();
+
+	return (sqrt((x*x) + (y*y)));
+}
+
 bool bsp( Point const a, Point const b, Point const c, Point const point) {
 	if ((point.getX() == a.getX() && point.getY() == a.getY())
 	|| (point.getX() == b.getX() && point.getY() == b.getY())
-	|| (point.getX() == c.getX() && point.getY() == c.getY()))
+	|| (point.getX() == c.getX() && point.getY() == c.getY()))	// temporary : until range between points function is made
 		return false;
+	
 	return true;
 }
