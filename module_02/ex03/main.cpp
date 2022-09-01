a52cf0cc1ee299a86edb6819ec6af7dafa9636c6
Pierre-marie LAM <plam@e1r5p16.clusters.42paris.fr>
2022-09-01 13:53:42 +0200

    some modifications on module_02

--- module_02/ex03/main.cpp
+++ module_02/ex03/main.cpp
@@ -6,27 +6,31 @@
 /*   By: plam <plam@student.42.fr>                  +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/06/02 12:31:38 by plam              #+#    #+#             */
-/*   Updated: 2022/06/09 00:34:29 by plam             ###   ########.fr       */
+/*   Updated: 2022/09/01 13:47:17 by plam             ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include <iostream>
 #include "Fixed.hpp"
+#include "Point.hpp"
 
 int	main( void ) {
-	Fixed		a;
-	Fixed const	b( Fixed( 5.05f ) * Fixed( 2 ) );
-	
-	std::cout << a << std::endl;
-	std::cout << ++a << std::endl;
-	std::cout << a << std::endl;
-	std::cout << a++ << std::endl;
-	std::cout << a << std::endl;
+	Point *a, *b, *c;
+	Point *point;
 
-	std::cout << b << std::endl;
+	a = new Point(12.5, 2.6);
+	b = new Point(-3.2, 4.35);
+	c = new Point(-7.8, 9.7);
 
-	std::cout << Fixed::min( a, b ) << std::endl;
-	std::cout << Fixed::max( a, b ) << std::endl;
+	point = new Point(10.4, 5.1);
 
+	if (bsp(a, b, c,point) == true)
+		std::cout << "True" << std::endl;
+	else
+		std::cout << "False" << std::endl;
+	delete a;
+	delete b;
+	delete a;
+	delete point;
 	return 0;
 }
