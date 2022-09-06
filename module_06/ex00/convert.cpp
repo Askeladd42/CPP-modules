8d4033533b05e9759578b5e6f6f79b726ed6bb85
Pierre-marie LAM <plam@e1r10p16.clusters.42paris.fr>
2022-09-06 17:43:39 +0200

    starting module_06

--- module_06/ex00/convert.cpp
+++ module_06/ex00/convert.cpp
@@ -6,14 +6,14 @@
 /*   By: plam <plam@student.42.fr>                  +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/08/29 12:47:52 by plam              #+#    #+#             */
-/*   Updated: 2022/09/06 17:36:41 by plam             ###   ########.fr       */
+/*   Updated: 2022/09/06 17:43:23 by plam             ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "convert.hpp"
 
 convert::convert( std::string elm ) {
-
+	this->toConvert = elm;
 }
 
 convert::~convert() { }
