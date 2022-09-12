aa5d18e4427ad0be41eb47807b3a2ee58e9bf3bd
Pierre-marie LAM <plam@e1r12p5.clusters.42paris.fr>
2022-09-12 15:11:51 +0200

    issues zith diamondtrap in the functions used from scav 1 fragtrap

--- module_03/ex03/DiamondTrap.cpp
+++ module_03/ex03/DiamondTrap.cpp
@@ -6,13 +6,13 @@
 /*   By: plam <plam@student.42.fr>                  +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/06/13 16:47:46 by plam              #+#    #+#             */
-/*   Updated: 2022/09/12 13:51:57 by plam             ###   ########.fr       */
+/*   Updated: 2022/09/12 15:08:10 by plam             ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "DiamondTrap.hpp"
 
-DiamondTrap::DiamondTrap( std::string name ) : ClapTrap(std::string(name).append("_clap_name")), _name(name) {
+DiamondTrap::DiamondTrap( std::string name ) : ClapTrap(std::string(name).append("_clap_name")), ScavTrap(name), FragTrap(name), _name(name) {
 	std::cout << "Default DiamondTrap " << this->_name << " constructor called" << std::endl;
 	FragTrap::setHP(100);
 	ScavTrap::setEP(50);
@@ -23,7 +23,7 @@ DiamondTrap::~DiamondTrap( void ) {
 	std::cout <<  "Default DiamondTrap " << this->_name << " destructor called" << std::endl;
 }
 
-DiamondTrap::DiamondTrap( DiamondTrap const &other ) : ClapTrap(other) {
+DiamondTrap::DiamondTrap( DiamondTrap const &other ) : ClapTrap(other), ScavTrap(other), FragTrap(other) {
 	std::cout << "Copy DiamondTrap " << this->_name << " constructor called" << std::endl;
 }
 
@@ -36,50 +36,6 @@ DiamondTrap	&DiamondTrap::operator=( DiamondTrap const &other ) {
 		this->_attackDamage = other._attackDamage;
 	}
 	return *this;
-}
-
-void	DiamondTrap::attack( const std::string& target ) {
-	if (this->_energyPoints == 0)
-		std::cout << this->_name << " has no energy to attack!" << std::endl;
-	if (this->_hitPoints <= 0) 
-		std::cout << this->_name << " is destroyed! It cannot attack!" << std::endl;
-	else {
-		this->_energyPoints--;
-		std::cout << "DiamondTrap " << this->_name << " attacks " << target 
-		<< ", causing " << this->_attackDamage << " points of damage!" << std::endl;
-	}
-}
-
-void	DiamondTrap::takeDamage( unsigned int amount ) {
-	this->_hitPoints -= amount;
-	std::cout << "DiamondTrap " << this->_name << " takes " << amount 
-		<< " points of damage!" << std::endl;
-	if (this->_hitPoints <= 0)
-		std::cout << "DiamondTrap " << this->_name << " is destroyed!" << std::endl;
-}
-
-void	DiamondTrap::beRepaired( unsigned int amount ) {
-	if (this->_energyPoints == 0)
-		std::cout << "DiamondTrap " << this->_name << " has not enough energy for reparations!" << std::endl;
-	if (this->_hitPoints <= 0)
-		std::cout << "DiamondTrap " << this->_name << " is destroyed! It cannot repair itself!" << std::endl;
-	else {
-		this->_hitPoints += amount;
-		this->_energyPoints--;
-		std::cout << "DiamondTrap " << this->_name << " regains " << amount 
-			<< " hit points!" << std::endl;
-	}
-}
-
-void	DiamondTrap::guardGate( void ) {
-	std::cout << "DiamondTrap " << this->_name << " has entered in Guard mode!" << std::endl;
-}
-
-void	DiamondTrap::highFivesGuys( void ) {
-	if (this->_energyPoints > 0) {
-		std::cout << this->_name << " : C'mon guys ! Give me a five !" << std::endl;
-		this->_energyPoints--;
-	}
 }
 
 void	DiamondTrap::whoAmI( void ) {
--- module_03/ex03/DiamondTrap.hpp
+++ module_03/ex03/DiamondTrap.hpp
@@ -6,7 +6,7 @@
 /*   By: plam <plam@student.42.fr>                  +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/06/13 16:31:38 by plam              #+#    #+#             */
-/*   Updated: 2022/09/12 13:47:32 by plam             ###   ########.fr       */
+/*   Updated: 2022/09/12 15:07:38 by plam             ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -28,14 +28,7 @@ public:
 	DiamondTrap( DiamondTrap const &other );
 	DiamondTrap	&operator=( DiamondTrap const &other );
 
-	void	attack( const std::string& target );
-	void	takeDamage( unsigned int amount );
-	void	beRepaired( unsigned int amount );
-	void	guardMode( void );
-	void	highFivesGuys( void );
 	void	whoAmI( void );
-
-	void	displayStatus( void );
 };
 
 
