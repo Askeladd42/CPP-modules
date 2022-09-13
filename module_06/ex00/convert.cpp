33605fd76ecbc2f600d9bfeb949a69bb24782766
Pierre-marie LAM <plam@e1r2p22.clusters.42paris.fr>
2022-09-13 15:03:39 +0200

    module_06 started

--- module_06/ex00/convert.cpp
+++ module_06/ex00/convert.cpp
@@ -6,14 +6,14 @@
 /*   By: plam <plam@student.42.fr>                  +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/08/29 12:47:52 by plam              #+#    #+#             */
-/*   Updated: 2022/09/06 17:43:23 by plam             ###   ########.fr       */
+/*   Updated: 2022/09/13 15:02:53 by plam             ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "convert.hpp"
 
 convert::convert( std::string elm ) {
-	this->toConvert = elm;
+	this->_toConvert = elm;
 }
 
 convert::~convert() { }
@@ -27,4 +27,43 @@ convert&	convert::operator=(convert const &other ) {
 		/* to complete */
 	}
 	return *this;
+}
+
+void	convert::setChar( std::string s ) {
+
+}
+
+void	convert::setInt( std::string s ) {
+
+}
+
+void	convert::setFloat( std::string s ) {
+
+}
+
+void	convert::setDouble( std::string s ) {
+	
+}
+
+char const		convert::getChar() const {
+
+}
+
+int const		convert::getInt() const {
+
+}
+
+float const		convert::getFloat() const {
+
+}
+
+double const	convert::getDouble() const {
+	
+}
+
+void	convert::getConvert() {
+	this->getChar();
+	this->getInt();
+	this->getFloat();
+	this->getDouble();
 }
--- module_06/ex00/convert.hpp
+++ module_06/ex00/convert.hpp
@@ -6,7 +6,7 @@
 /*   By: plam <plam@student.42.fr>                  +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/08/29 12:46:04 by plam              #+#    #+#             */
-/*   Updated: 2022/09/06 17:43:20 by plam             ###   ########.fr       */
+/*   Updated: 2022/09/13 15:02:36 by plam             ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -14,22 +14,40 @@
 # define __CONVERT_HPP__
 
 # include <iostream>
+# include <iomanip>
 # include <cmath>
+# include <limits>
 
 class convert
 {
 private:
-	std::string	toConvert;
+	std::string		_toConvert;
+
+	bool			_isChar;
+	bool			_isInt;
+	bool			_isFloat;
+	bool			_isDouble;
+
+	float			_float;
+	double			_double;
+
+	void			setChar( std::string s );
+	void			setInt( std::string s );
+	void			setFloat( std::string s );
+	void			setDouble( std::string s );
+
+	char const		getChar() const;
+	int const		getInt() const;
+	float const		getFloat() const;
+	double const	getDouble() const;
+
 public:
 	convert( std::string elm );
 	~convert();
 	convert( convert const &other );
 	convert	&operator=( convert const &other );
 
-	char const		getChar() const;
-	int const		getInt() const;
-	float const		getFloat() const;
-	double const	getDouble() const;
+	void			getConvert();
 };
 
 #endif
