26fedffcfaaa4da1fee432b1b7ad55046774f3c5
Pierre-marie LAM <plam@e1r3p19.clusters.42paris.fr>
2022-09-15 14:09:39 +0200

    verifying leaks in main files

--- module_04/ex00/Cat.hpp
+++ module_04/ex00/Cat.hpp
@@ -6,7 +6,7 @@
 /*   By: plam <plam@student.42.fr>                  +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/06/23 15:01:27 by plam              #+#    #+#             */
-/*   Updated: 2022/06/24 17:34:06 by plam             ###   ########.fr       */
+/*   Updated: 2022/09/15 11:56:07 by plam             ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -23,7 +23,7 @@ public:
 	Cat( Cat const &other );
 	Cat	&operator=( Cat const &other );
 
-	virtual void	makeSound( void ) const;
+	void	makeSound( void ) const;
 };
 
 #endif
