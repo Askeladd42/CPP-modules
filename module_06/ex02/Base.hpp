55cbca9384d2522269b11debcec6ab315a6640f4
Pierre-marie LAM <plam@e1r4p2.clusters.42paris.fr>
2022-09-27 17:02:23 +0200

    ex02 started

--- module_06/ex02/Base.hpp
+++ module_06/ex02/Base.hpp
@@ -6,13 +6,20 @@
 /*   By: plam <plam@student.42.fr>                  +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/09/27 15:59:52 by plam              #+#    #+#             */
-/*   Updated: 2022/09/27 16:48:51 by plam             ###   ########.fr       */
+/*   Updated: 2022/09/27 17:00:55 by plam             ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #ifndef __BASE_HPP__
 # define __BASE_HPP__
 
+class Base {
+public:
+	virtual ~Base();
+};
 
+Base	*generate();
+void	identify( Base* p );
+void	identify( Base& p );
 
 #endif
