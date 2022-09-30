e6ae5a55aba516779a7d989931d7e95783f860e9
Pierre-marie LAM <plam@e1r3p17.clusters.42paris.fr>
2022-09-30 15:24:44 +0200

    issues in ex01 main, need fixes

--- module_07/ex01/main.cpp
+++ module_07/ex01/main.cpp
@@ -6,7 +6,7 @@
 /*   By: plam <plam@student.42.fr>                  +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/09/30 12:44:58 by plam              #+#    #+#             */
-/*   Updated: 2022/09/30 15:19:03 by plam             ###   ########.fr       */
+/*   Updated: 2022/09/30 15:24:14 by plam             ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -14,13 +14,13 @@
 
 int	main( void ) {
 	int			int_tab[] = { -12, 42, -2147483648, 2147483647, 0, -54154655, -13, 1 };
-	std:string	str_tab[] = { "Salut !", "C'est Billy !", "On a des problèmes là !" };
+	std::string	str_tab[] = { "Salut !", "C'est Billy !", "On a des problèmes là !" };
 	float		float_tab[] = { -42.3f, 27.18f, 145421535453435.75454f, -534354354354 };
 	double		double_tab[] = { 21.42, -5431134565, 45533345334.54245346546 };
 
 	iter(int_tab, 8, abs);
 	iter(str_tab, 3, abs);
-	iter(float_tab, 4, abs);
-	iter(double_tab, 3, abs);
+	iter(float_tab, 4, fabs);
+	iter(double_tab, 3, fabs);
 	return 0;
 }
