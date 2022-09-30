a04453586e0f3058d852ecc585ec663913f7838a
Askeladd42 <piema.lam@gmail.com>
2022-09-30 15:58:14 +0200

    issues with types and arguments in main for tests, need fixes

--- module_07/ex01/Iter.hpp
+++ module_07/ex01/Iter.hpp
@@ -6,14 +6,15 @@
 /*   By: plam <plam@student.42.fr>                  +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/09/30 12:41:07 by plam              #+#    #+#             */
-/*   Updated: 2022/09/30 15:15:59 by plam             ###   ########.fr       */
+/*   Updated: 2022/09/30 15:54:22 by plam             ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #ifndef __ITER_HPP__
 # define __ITER_HPP__
-#include <iostream>
+# include <iostream>
 # include <cstdlib>
+# include <cmath>
 
 template < typename T >
 void	iter(T const * array, size_t size, void (*f)(T const &)) {
