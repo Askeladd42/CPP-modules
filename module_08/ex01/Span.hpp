23c2135b7490fdf203593383d10a8ad4c1850a96
Pierre-marie LAM <plam@e1r5p14.42.fr>
2022-10-05 12:43:27 +0200

    added a makefile for ex00 of module_08

---
+++ module_08/ex01/Span.hpp
@@ -0,0 +1,28 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   Span.hpp                                           :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: plam <plam@student.42.fr>                  +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2022/10/04 16:58:16 by plam              #+#    #+#             */
+/*   Updated: 2022/10/05 12:43:14 by plam             ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#ifndef __SPAN_HPP__
+# define __SPAN_HPP__
+
+# include <algorithm>
+# include <vector> 
+
+template<typename T>
+class Span {
+	private:
+		/* data */
+	public:
+		Span(/* args */);
+		~Span();
+};
+
+#endif
