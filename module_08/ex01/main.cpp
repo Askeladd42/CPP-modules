99a8e26d300f1261a0218272427452e40dc5a2fa
Pierre-marie LAM <plam@e2r6p7.clusters.42paris.fr>
2022-10-10 15:00:12 +0200

    ex02 started

--- module_08/ex01/main.cpp
+++ module_08/ex01/main.cpp
@@ -6,7 +6,7 @@
 /*   By: plam <plam@student.42.fr>                  +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/10/04 16:35:25 by plam              #+#    #+#             */
-/*   Updated: 2022/10/10 00:45:04 by plam             ###   ########.fr       */
+/*   Updated: 2022/10/10 15:00:04 by plam             ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -34,10 +34,22 @@ int main()
 	srand(time(NULL) + rand() % 1000);
 	unsigned int	i = 0;
 	while (i++ < sp0.getSize())
-		sp.addNumber(rand());
+		sp0.addNumber(rand());
 	try {
-		std::cout << sp.shortestSpan() << std::endl;
-		std::cout << sp.longestSpan() << std::endl;
+		std::cout << sp0.shortestSpan() << std::endl;
+		std::cout << sp0.longestSpan() << std::endl;
+	}
+	catch(std::exception &e) {
+		std::cerr << e.what() <<std::endl;
+	}
+
+	Span	spExtra = Span(100000);
+	unsigned int	i = 0;
+	while (i++ < spExtra.getSize())
+		spExtra.addNumber(rand());
+	try {
+		std::cout << spExtra.shortestSpan() << std::endl;
+		std::cout << spExtra.longestSpan() << std::endl;
 	}
 	catch(std::exception &e) {
 		std::cerr << e.what() <<std::endl;
