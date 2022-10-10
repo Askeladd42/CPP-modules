2ac500e9358273f5fcbd36316bb1fbe3bbf689ec
Pierre-marie LAM <plam@e2r6p7.clusters.42paris.fr>
2022-10-10 15:26:03 +0200

    ex02 started

--- module_08/ex02/main.cpp
+++ module_08/ex02/main.cpp
@@ -6,13 +6,38 @@
 /*   By: plam <plam@student.42.fr>                  +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/10/04 16:35:25 by plam              #+#    #+#             */
-/*   Updated: 2022/10/10 00:50:31 by plam             ###   ########.fr       */
+/*   Updated: 2022/10/10 15:25:36 by plam             ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "MutantStack.hpp"
 
-int main()
-{
+int main() {
+	MutantStack<int> mstack;
+	mstack.push(5);
+	mstack.push(17);
+
+	std::cout << mstack.top() << std::endl;
+	
+	mstack.pop();
+
+	std::cout << mstack.size() << std::endl;
+
+	mstack.push(3);
+	mstack.push(5);
+	mstack.push(737);
+	//[...]
+	mstack.push(0);
+	
+	MutantStack<int>::iterator it = mstack.begin();
+	MutantStack<int>::iterator ite = mstack.end();
+
+	++it;
+	--it;
+	while (it != ite) {
+		std::cout << *it << std::endl;
+		++it;
+	}
+	std::stack<int> s(mstack);
 	return 0;
 }
