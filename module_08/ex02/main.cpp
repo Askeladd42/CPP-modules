5ad3fae0a54bbaf9bd9a8e2cad89a6f08bc4a19b
Pierre-marie LAM <plam@e1r10p7.clusters.42paris.fr>
2022-10-13 14:25:56 +0200

    module_08 finished, need more tests/examples for the main to illustrate the MutantStack use

--- module_08/ex02/main.cpp
+++ module_08/ex02/main.cpp
@@ -6,13 +6,15 @@
 /*   By: plam <plam@student.42.fr>                  +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/10/04 16:35:25 by plam              #+#    #+#             */
-/*   Updated: 2022/10/10 15:25:36 by plam             ###   ########.fr       */
+/*   Updated: 2022/10/13 14:25:46 by plam             ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "MutantStack.hpp"
 
 int main() {
+
+	std::cout << "######### MutantStack basic tests #########" << std::endl;
 	MutantStack<int> mstack;
 	mstack.push(5);
 	mstack.push(17);
@@ -38,6 +40,24 @@ int main() {
 		std::cout << *it << std::endl;
 		++it;
 	}
+
+	std::cout << "######### Stack copy of MutantStack basic tests #########" << std::endl;
+
 	std::stack<int> s(mstack);
+	s.push(5);
+	s.push(17);
+
+	std::cout << s.top() << std::endl;
+	
+	s.pop();
+
+	std::cout << s.size() << std::endl;
+
+	s.push(3);
+	s.push(5);
+	s.push(737);
+	//[...]
+	s.push(0);
+
 	return 0;
 }
