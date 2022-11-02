7b455b3ba9d2ba991256401811d86b37fe024da0
Pierre-marie LAM <plam@e2r10p8.clusters.42paris.fr>
2022-11-02 17:20:07 +0100

    finished, but need fixes to match the ex01 suject on the addNumber case with many adds, also more detailed tests and examples for the last ex

--- module_08/ex01/Span.cpp
+++ module_08/ex01/Span.cpp
@@ -6,7 +6,7 @@
 /*   By: plam <plam@student.42.fr>                  +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/10/06 17:37:35 by plam              #+#    #+#             */
-/*   Updated: 2022/11/02 13:24:59 by plam             ###   ########.fr       */
+/*   Updated: 2022/11/02 17:18:50 by plam             ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -29,7 +29,7 @@ Span&				Span::operator=( Span const &other ) {
 	return *this;
 }
 
-void				Span::addNumber( int newNb ) {
+void				Span::addNumber( int newNb ) {	// to redone without iterative calls for the addNumber pecified in the subject
 	if (this->_stock.size() == this->_size)
 		throw AlreadyFull;
 	else
