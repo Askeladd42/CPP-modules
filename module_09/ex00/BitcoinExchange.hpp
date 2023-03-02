8eb7c40772b68aa286ffdc6471f4313fc1558302
Pierre-marie LAM <plam@e1r7p7.clusters.42paris.fr>
2023-03-02 12:56:20 +0100

    started the bonus module 09 for CPP pool

--- module_09/ex00/BitcoinExchange.hpp
+++ module_09/ex00/BitcoinExchange.hpp
@@ -6,13 +6,15 @@
 /*   By: plam <plam@student.42.fr>                  +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/03/02 12:30:45 by plam              #+#    #+#             */
-/*   Updated: 2023/03/02 12:31:40 by plam             ###   ########.fr       */
+/*   Updated: 2023/03/02 12:56:13 by plam             ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #ifndef __BITCOINEXHCANGE_HPP__
 # define __BITCOINEXHCANGE_HPP__
 
-#include <vector>
+# include <iostream>
+# include <stdexcept>
+# include <vector>
 
 #endif
